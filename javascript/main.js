42e5bda742acaa62719bcbef76f09fbc12d89d6a
susha12 <87581584+susha12@users.noreply.github.com>
2022-04-30 18:44:57 +0400

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -21,27 +21,21 @@ btnNext.onclick = ()=>{
         sliderBox[i+2].classList.remove('active');
     }
 }
-
 btnBack.onclick = ()=>{
     if(i == 0){
         sliderBox[i+2].classList.add('active');
         sliderBox[i].classList.remove('active');
         i+=2;
-        console.log(i)
     }
      else  if(i == 1){
          i=0;
         sliderBox[i].classList.add('active');
         sliderBox[i+1].classList.remove('active');
-       
-        console.log(i)
     }
     else  if(i ==2){
         i=1;
         sliderBox[i].classList.add('active');
         sliderBox[i+1].classList.remove('active');
-        console.log(i)
     }
-    
 }
 
